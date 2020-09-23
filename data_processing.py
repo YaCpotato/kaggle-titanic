e88007c0a99c3486f4cbd3a4ade2a60a65558137
YaCpotato <yasshisshy@gmail.com>
2020-09-23 12:56:44 +0900

    中間ファイル作成

---
+++ data_processing.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+
+def row_read():
+    """
+    生データを読み込んで返すメソッド
+    """
+    train = pd.read_csv('train.csv')
+    test = pd.read_csv('test.csv')
+    return train,test
+
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+sample = pd.read_csv('gender_submission.csv')
+
+data = pd.concat([train, test], sort = False)
+data['Sex'].replace(['male', 'female'],[0,1],inplace = True)
+data['Fare'].fillna(np.mean(data['Fare']), inplace = True)
+data['Embarked'].fillna(('S'), inplace=True)
+data['Embarked'] = data['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
+age_avg = data['Age'].mean()
+age_std = data['Age'].std()
+data['Age'].fillna(np.random.randint(age_avg - age_std, age_avg + age_std), inplace=True)
+delete_columns = ['Name', 'PassengerId', 'SibSp', 'Parch', 'Ticket', 'Cabin']
+data.drop(delete_columns, axis=1, inplace=True)
+
+train = data[:len(train)]
+test = data[len(train):]
+
+Y_train = train['Survived']
+X_train = train.drop('Survived', axis=1)
+Y_test = test['Survived']
+X_test = test.drop('Survived', axis=1)
