69d022c2668ce868fe295242e88bc1aacaf9ab3f
YaCpotato <yasshisshy@gmail.com>
2020-09-24 16:00:03 +0900

    モジュール整理完了

--- data_processing.py
+++ data_processing.py
@@ -16,20 +16,22 @@ def processed_data():
     train,test = row_read()
     # あとから再度trainとtestに戻すので、並べかえはしない
     data = pd.concat([train, test], sort = False)
-data['Sex'].replace(['male', 'female'],[0,1],inplace = True)
-data['Fare'].fillna(np.mean(data['Fare']), inplace = True)
-data['Embarked'].fillna(('S'), inplace=True)
-data['Embarked'] = data['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
-age_avg = data['Age'].mean()
-age_std = data['Age'].std()
-data['Age'].fillna(np.random.randint(age_avg - age_std, age_avg + age_std), inplace=True)
-delete_columns = ['Name', 'PassengerId', 'SibSp', 'Parch', 'Ticket', 'Cabin']
-data.drop(delete_columns, axis=1, inplace=True)
+    data['Sex'].replace(['male', 'female'],[0,1],inplace = True)
+    data['Fare'].fillna(np.mean(data['Fare']), inplace = True)
+    data['Embarked'].fillna(('S'), inplace=True)
+    data['Embarked'] = data['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
+    age_avg = data['Age'].mean()
+    age_std = data['Age'].std()
+    data['Age'].fillna(np.random.randint(age_avg - age_std, age_avg + age_std), inplace=True)
+    delete_columns = ['Name', 'PassengerId', 'SibSp', 'Parch', 'Ticket', 'Cabin']
+    data.drop(delete_columns, axis=1, inplace=True)
 
-train = data[:len(train)]
-test = data[len(train):]
+    train = data[:len(train)]
+    test = data[len(train):]
 
-Y_train = train['Survived']
-X_train = train.drop('Survived', axis=1)
-Y_test = test['Survived']
-X_test = test.drop('Survived', axis=1)
+    Y_train = train['Survived']
+    X_train = train.drop('Survived', axis=1)
+    Y_test = test['Survived']
+    X_test = test.drop('Survived', axis=1)
+    
+    return X_train,Y_train,X_test,Y_test
--- run.py
+++ run.py
@@ -3,28 +3,9 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix,classification_report
 
-train = pd.read_csv('train.csv')
-test = pd.read_csv('test.csv')
-sample = pd.read_csv('gender_submission.csv')
+import data_processing
 
-data = pd.concat([train, test], sort = False)
-data['Sex'].replace(['male', 'female'],[0,1],inplace = True)
-data['Fare'].fillna(np.mean(data['Fare']), inplace = True)
-data['Embarked'].fillna(('S'), inplace=True)
-data['Embarked'] = data['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
-age_avg = data['Age'].mean()
-age_std = data['Age'].std()
-data['Age'].fillna(np.random.randint(age_avg - age_std, age_avg + age_std), inplace=True)
-delete_columns = ['Name', 'PassengerId', 'SibSp', 'Parch', 'Ticket', 'Cabin']
-data.drop(delete_columns, axis=1, inplace=True)
-
-train = data[:len(train)]
-test = data[len(train):]
-
-Y_train = train['Survived']
-X_train = train.drop('Survived', axis=1)
-Y_test = test['Survived']
-X_test = test.drop('Survived', axis=1)
+X_train,Y_train,X_test,Y_test = data_processing.processed_data()
 
 clf = LogisticRegression(penalty = 'l2', solver = 'sag', random_state = 0, verbose = 1)
 clf.fit(X_train, Y_train)
