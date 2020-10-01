e58f66586df49aea39ead57657b5552c19ecf2e8
YaCpotato <yasshisshy@gmail.com>
2020-10-01 11:17:20 +0900

    いつやったかわすれた

--- data_view.py
+++ data_view.py
@@ -4,6 +4,9 @@ import pandas_profiling as pdp
 import data_processing
 
 X_train,Y_train,X_test,Y_test = data_processing.processed_data()
+df = pd.read_csv('train.csv')
 
+pdf = pdp.ProfileReport(df)
 profile = pdp.ProfileReport(X_train)
-profile.to_file("train.html")
+profile.to_file('train_after_creansing.html')
+pdf.to_file('train.html')
